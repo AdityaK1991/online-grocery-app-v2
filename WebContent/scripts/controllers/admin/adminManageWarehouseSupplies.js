6653b6872244f40fe25e70b24137f3ae698fe33a
AdityaK1991 <kulkarni.aditya.p@gmail.com>
2016-12-10 02:19:15 -0600

    updated suppliers module

--- WebContent/scripts/controllers/admin/adminManageWarehouseSupplies.js
+++ WebContent/scripts/controllers/admin/adminManageWarehouseSupplies.js
@@ -11,14 +11,26 @@ angular.module('onlineGroceryStoreApp')
     $rootScope.isAdminMenuVisible = true;
   
   	$scope.products= [{
-  		name:'Rice', size:150, qty:60, supplier:'S1', price:25
+  		name:'Apple', size:150, qty:60, supplier:'S1', price:25
   	},
   	{
-  		name:'Spices', size:100, qty:30, supplier:'S2', price:40
+  		name:'Apple pie', size:100, qty:30, supplier:'S2', price:40
   	},
   	{
-  		name:'Tomatoes', size:20, qty:100, supplier:'S3', price:15
-  	}]
+  		name:'Tomatoes', size:20, qty:100, supplier:'S3', price:60
+  	},
+  	{
+  		name:'Coke', size:22, qty:150, supplier:'S3', price:55
+  	},
+  	{
+  		name:'Carrot', size:36, qty:200, supplier:'S3', price:60
+  	},
+  	{
+  		name:'Tomatoes', size:28, qty:220, supplier:'S3', price:65
+  	},
+  	{
+  		name:'Beans', size:25, qty:250, supplier:'S3', price:70
+  	}];
 
   	$scope.cartProducts = [];
 
